6f536c45c0676820b54c5d0fa21761483b329fc2
Cris Meta <0x2faced@gmail.com>
2022-04-16 16:41:04 +0900

    feat: query with proxy

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # Opensea Unofficial API
 
-## Example
+<img alt="Deno" src="https://img.shields.io/badge/-Deno-gray?logo=deno&labelColor=black&style=flat-square" />
+<img alt="Opensea Unofficial Version" src="https://img.shields.io/github/v/tag/0x2faced/opensea-unofficial?style=flat-square" />
+
+## Usage
 
 ```ts
-import { query } from "https://raw.githubusercontent.com/0x2faced/opensea-unofficial/v0.0.1/opensea.ts";
+import { query } from "https://raw.githubusercontent.com/0x2faced/opensea-unofficial/main/opensea.ts";
+
+// specific version
+// import { query } from "https://raw.githubusercontent.com/0x2faced/opensea-unofficial/v0.0.1/opensea.ts";
 
 const { query: { search } } = await query("AssetSearchQuery", {
   collection: "ujin",
@@ -21,4 +27,32 @@ const { query: { search } } = await query("AssetSearchQuery", {
 });
 
 console.log(search); // show your opensea list
+```
+
+### with Proxy
+
+```ts
+const client = Deno.createHttpClient({/* ... */});
+
+const { query: { search } } = await query({
+  id: "AssetSearchQuery",
+  variables: {
+    collection: "ujin",
+    collectionQuery: null,
+    collectionSortBy: "SEVEN_DAY_VOLUME",
+    collections: [
+      "ujin",
+    ],
+    count: 32,
+    resultModel: "ASSETS",
+    showContextMenu: true,
+    shouldShowQuantity: false,
+    sortAscending: false,
+    sortBy: "CREATED_DATE",
+  },
+  // type of init => second fetch parameters
+  init: {
+    client,
+  },
+});
 ```
--- opensea.ts
+++ opensea.ts
@@ -1,21 +1,51 @@
 import { QueryMap, QueryVaraiblesMap } from "./types.ts";
 import { queries } from "./graphql_api_requests.ts";
 
-export async function query<TId extends keyof QueryVaraiblesMap>(
+export type QueryRequstInit = NonNullable<Parameters<typeof fetch>[1]>;
+
+export interface QueryParams<TId extends keyof QueryVaraiblesMap> {
+  id: TId;
+  variables: QueryVaraiblesMap[TId];
+  init?: QueryRequstInit;
+}
+
+export function query<TId extends keyof QueryVaraiblesMap>(
   id: TId,
   variables: QueryVaraiblesMap[TId],
+): Promise<QueryMap[TId]>;
+export function query<TId extends keyof QueryVaraiblesMap>(
+  params: QueryParams<TId>,
+): Promise<QueryMap[TId]>;
+export async function query<TId extends keyof QueryVaraiblesMap>(
+  idOrParams: TId | QueryParams<TId>,
+  variables?: QueryVaraiblesMap[TId],
 ): Promise<QueryMap[TId]> {
+  let id: TId;
+  let init: QueryRequstInit = {};
+  if (typeof idOrParams === "string") {
+    id = idOrParams;
+  } else {
+    id = idOrParams.id;
+    variables = idOrParams.variables;
+    if (idOrParams.init) {
+      init = idOrParams.init;
+    }
+  }
+
+  const { headers, ...remainInit } = init ?? {};
   const response = await fetch("https://api.opensea.io/graphql/", {
     method: "POST",
     headers: {
       "content-type": "application/json",
       "x-signed-query": queries[id].signedKey,
+      ...headers,
     },
     body: JSON.stringify({
       id,
       query: queries[id].query,
       variables,
     }),
+    ...remainInit,
   });
   if (response.status === 200) {
     return await response.json().then((
